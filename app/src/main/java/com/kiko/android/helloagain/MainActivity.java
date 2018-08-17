b4ec5c656c4aae1cd35644c97b36216e2d3b98dc
Renato Luppi <renatoluppi@gmail.com>
2018-08-17 06:27:05 -0300

    inseri o botao na pagina um e linkei pra pagina 2

--- app/src/main/java/com/kiko/android/helloagain/MainActivity.java
+++ app/src/main/java/com/kiko/android/helloagain/MainActivity.java
@@ -1,7 +1,10 @@
 package com.kiko.android.helloagain;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+
+        Button button1 = (Button) findViewById(R.id.button1);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                abrepg2();
+            }
+        });
+
     }
+
+    private void abrepg2(){
+        Intent intent = new Intent(this, Tela2Activity.class);
+        startActivity(intent);
+    }
+
 }
