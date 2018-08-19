cfad25d41bc1a5abb171a722225e7b07bf725d20
Renato Luppi <renatoluppi@gmail.com>
2018-08-19 18:52:48 -0300

    criei um numero randomico e um Log.d para ver o numero que foi gerado. Agora precisa ver como passar este numero para próxima tela e exibir ele

--- app/src/main/java/com/kiko/android/helloagain/MainActivity.java
+++ app/src/main/java/com/kiko/android/helloagain/MainActivity.java
@@ -3,8 +3,11 @@ package com.kiko.android.helloagain;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +17,17 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
 
-
+        String numero1;
         Button button1 = (Button) findViewById(R.id.button1);
 
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 abrepg2();
+                Random rand = new Random();
+                int sorteado = rand.nextInt(60 - 1);
+                String numero1 = new Integer(sorteado).toString();
+                Log.d(numero1, "onClick: ");
             }
         });
 
@@ -28,6 +35,7 @@ public class MainActivity extends AppCompatActivity {
 
     private void abrepg2(){
         Intent intent = new Intent(this, Tela2Activity.class);
+
         startActivity(intent);
     }
 
