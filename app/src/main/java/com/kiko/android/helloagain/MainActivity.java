eaed86a94ccbfeb2476b3384ee9f360ebd2662ef
Renato Luppi <renatoluppi@gmail.com>
2018-08-25 23:23:15 -0300

    criei os 6 botoes, coloquei na vertical, modifiquei o range para nao ter zero nunca agora falta evitar ter numeros repetidos e colocar eles em ordem crescente

--- app/src/main/java/com/kiko/android/helloagain/MainActivity.java
+++ app/src/main/java/com/kiko/android/helloagain/MainActivity.java
@@ -32,10 +32,35 @@ public class MainActivity extends AppCompatActivity {
     private void abrepg2(){
         Intent intent = new Intent(this, Tela2Activity.class);
         Random rand = new Random();
-        int sorteado = rand.nextInt(60 - 1);
+
+        int sorteado = rand.nextInt(60) + 1;
         String numero1 = new Integer(sorteado).toString();
 
         intent.putExtra("KEY",numero1);
+
+        int sorteado2 = rand.nextInt(60 ) + 1;
+        String numero2 = new Integer(sorteado2).toString();
+        intent.putExtra("KEY2",numero2);
+
+        int sorteado3 = rand.nextInt(60 ) + 1;
+        String numero3 = new Integer(sorteado3).toString();
+        intent.putExtra("KEY3",numero3);
+
+        int sorteado4 = rand.nextInt(60 ) + 1;
+        String numero4 = new Integer(sorteado4).toString();
+        intent.putExtra("KEY4",numero4);
+
+        int sorteado5 = rand.nextInt(60 ) + 1;
+        String numero5 = new Integer(sorteado5).toString();
+        intent.putExtra("KEY5",numero5);
+
+        int sorteado6 = rand.nextInt(60 ) + 1;
+        String numero6 = new Integer(sorteado6).toString();
+        intent.putExtra("KEY6",numero6);
+
+//todo nao pode ter numeros repetidos, criar um if equals para evitar isso
+//todo no resultado colocar os numeros em ordem crescente
+
         startActivity(intent);
     }
 
--- app/src/main/java/com/kiko/android/helloagain/Tela2Activity.java
+++ app/src/main/java/com/kiko/android/helloagain/Tela2Activity.java
@@ -14,11 +14,22 @@ public class Tela2Activity extends AppCompatActivity {
 
 
         TextView textoRecebe = (TextView) findViewById(R.id.textoRecebe);
+        TextView textoRecebe2 = (TextView) findViewById(R.id.textoRecebe2);
+        TextView textoRecebe3 = (TextView) findViewById(R.id.textoRecebe3);
+        TextView textoRecebe4 = (TextView) findViewById(R.id.textoRecebe4);
+        TextView textoRecebe5 = (TextView) findViewById(R.id.textoRecebe5);
+        TextView textoRecebe6 = (TextView) findViewById(R.id.textoRecebe6);
+
 
 
         Intent intent = getIntent();
 
+        textoRecebe.setText(intent.getStringExtra("KEY" ));
+        textoRecebe2.setText(intent.getStringExtra("KEY2" ));
+        textoRecebe3.setText(intent.getStringExtra("KEY3" ));
+        textoRecebe4.setText(intent.getStringExtra("KEY4" ));
+        textoRecebe5.setText(intent.getStringExtra("KEY5" ));
+        textoRecebe6.setText(intent.getStringExtra("KEY6" ));
 
-        textoRecebe.setText(intent.getStringExtra("KEY"));
     }
 }
